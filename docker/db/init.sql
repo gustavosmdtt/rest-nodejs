80bba432331874fe2b2bd6c30cf83dd598ecbc7a
gustavosmdtt <gustavoschmidt147@gmail.com>
2025-07-23 19:24:31 -0300

    feat: add docker to project

---
+++ docker/db/init.sql
@@ -0,0 +1,24 @@
+CREATE DATABASE IF NOT EXISTS store;
+USE store;
+
+CREATE TABLE IF NOT EXISTS users (
+  userId INT NOT NULL AUTO_INCREMENT,
+  email VARCHAR(55) NOT NULL,
+  password VARCHAR(255) NOT NULL,
+  PRIMARY KEY (userId)
+);
+
+CREATE TABLE IF NOT EXISTS products (
+  productId INT NOT NULL AUTO_INCREMENT,
+  title VARCHAR(45) NOT NULL,
+  price FLOAT NOT NULL,
+  PRIMARY KEY (productId)
+);
+
+CREATE TABLE IF NOT EXISTS orders (
+  orderId INT NOT NULL AUTO_INCREMENT,
+  quantity SMALLINT NOT NULL,
+  productId INT,
+  PRIMARY KEY (orderId),
+  FOREIGN KEY (productId) REFERENCES products(productId)
+);
