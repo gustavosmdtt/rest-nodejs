9d6e366217765fc03400eba84dc8dbb0d47b9571
gustavosmdtt <gustavoschmidt147@gmail.com>
2025-07-29 10:52:59 -0300

    feat: add new user api testing

---
+++ tests/cypress/api/users/login.cy.ts
@@ -0,0 +1,50 @@
+import { faker } from "@faker-js/faker";
+import { APIPayload } from "tests/cypress/types/types";
+import { UserPayload } from "tests/cypress/types/users";
+
+describe('User - Login API', () => {
+    const fakerEmail = faker.internet.email();
+    const fakerPass = faker.internet.password({ length: 8 });
+
+    const options: APIPayload<UserPayload> = {
+        method: 'POST',
+        url: 'usuarios/login',
+        failOnStatusCode: false,
+        body: {
+            email: fakerEmail,
+            password: fakerPass
+        }
+    }
+
+    before(() => {
+        const newUser: APIPayload<UserPayload> = {
+            ...options,
+            url: 'usuarios/cadastro',
+        }
+
+        cy.api_makeRequest(newUser);
+    })
+
+    it('should login an existing user', () => {        
+        cy.api_makeRequest(options).then((res) => {
+            expect(res.body.message).to.equal('Authentication successful');
+            expect(res.body.token).to.be.a('string');
+            expect(res.status).to.equal(200);
+        })
+    });
+
+    it('should not login with invalid credentials', () => {
+        const invalidOptions: APIPayload<UserPayload> = {
+            ...options,
+            body: {
+                email: fakerEmail,
+                password: 'wrongpassword'
+            }
+        }
+
+        cy.api_makeRequest(invalidOptions).then((res) => {
+            expect(res.body.message).to.equal('Invalid credentials');
+            expect(res.status).to.equal(401);
+        })
+    });
+});
---
+++ tests/cypress/api/users/register.cy.ts
@@ -0,0 +1,60 @@
+import { APIPayload } from "tests/cypress/types/types";
+import { UserPayload } from "tests/cypress/types/users";
+import { faker } from '@faker-js/faker';
+
+describe('User - Register API', () => {
+    const registryOptions: APIPayload<UserPayload> = {
+        method: 'POST',
+        url: 'usuarios/cadastro',
+        failOnStatusCode: false,
+        body: {
+            email: faker.internet.email(),
+            password: faker.internet.password({ length: 8 })
+        }
+    }
+
+    before(() => {
+        cy.api_makeRequest(registryOptions);
+    })
+
+    it('should not register with existing email', () => {
+        cy.api_makeRequest(registryOptions).then((res) => {
+            console.log(res);
+            
+            expect(res.status).to.equal(401);
+            expect(res.body.message).to.equal('User already registered');
+        });
+    });
+
+    it('should not register with invalid password', () => {
+        const newOptions = {
+            ...registryOptions,
+            body: {
+                email: faker.internet.email(),
+                password: '1234567'
+            }
+        }
+
+        cy.api_makeRequest(newOptions).then((res) => {
+            expect(res.status).to.equal(400);
+            expect(res.body.message).to.equal('Password must be at least 8 characters long');
+        });
+    });
+
+    it('should create a new user', () => {
+        const newOptions = {
+            ...registryOptions,
+            body: {
+                email: faker.internet.email(),
+                password: faker.internet.password({ length: 8 })
+            }
+        }
+
+        cy.api_makeRequest(newOptions).then((res) => {
+            expect(res.status).to.equal(201);
+            expect(res.body.message).to.equal('User successfully created');
+            expect(res.body.userCreated?.email).to.equal(newOptions.body.email);
+            expect(res.body.userCreated?.userId).to.be.a('number');
+        });
+    });
+});
