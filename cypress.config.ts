4c24314314ea1523f8507e9fb71ddf9f6a4cd67d
gustavosmdtt <gustavoschmidt147@gmail.com>
2025-07-29 10:48:11 -0300

    chore: config cypress test

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,9 +2,16 @@ import { defineConfig } from 'cypress';
 
 export default defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:3000',
-    setupNodeEvents(on, config) {
-      // setup de plugins aqui, se houver
-    },
-  },
+    baseUrl: 'http://localhost:3000/',
+    supportFile: 'tests/cypress/support/e2e.{js,jsx,ts,tsx}',
+    specPattern: [
+      'tests/cypress/api/**/*.cy.{js,jsx,ts,tsx}', 
+      'tests/cypress/e2e/**/*.cy.{js,jsx,ts,tsx}'
+    ],
+    fixturesFolder: 'tests/cypress/fixtures',
+    screenshotsFolder: 'tests/cypress/screenshots',
+    video: false,
+    watchForFileChanges: false,
+    setupNodeEvents(on, config) {},
+  }, 
 });
