d3a196abaaec690998161387f4a1c514941562c9
gustavosmdtt <gustavoschmidt147@gmail.com>
2025-08-14 10:59:44 -0300

    refactor: changes in middleware and controller user files

--- src/controllers/user-controller.js
+++ src/controllers/user-controller.js
@@ -1,110 +1,55 @@
-const mysql = require('../mysql').pool;
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken')
+const jwt = require('jsonwebtoken');
+const { createUser } = require('../services/userService');
 
-exports.register = (req, res, next) => {
-    mysql.getConnection((error, connection) => {
-        if (error) {
-            return res.status(500).send({ error: error })
-        }
+exports.register = async (req, res, next) => {
+    try {
+        const user = {
+            email: req.body.email,
+            password: req.body.password
+        };
 
-        connection.query(
-            'SELECT * FROM users WHERE email = ?',
-            [req.body.email],
-            (error, results) => {
-                if (error) {
-                    return res.status(500).send({ error: error })
-                }
+        const result = await createUser(req.dbConnection, user);
 
-                if (req.body.password.length < 8) {
-                    return res.status(400).send({ message: 'Password must be at least 8 characters long' })
-                }
-
-                if (results.length > 0) {
-                    res.status(401).send({ message: 'User already registered' })
-                } else {
-                    let numSalt = 10
-                    bcrypt.hash(req.body.password.toString(), numSalt, (errorBcrypt, hash) => {
-                        if (errorBcrypt) {
-                            return res.status(500).send({ error: errorBcrypt })
-                        }
-                        connection.query(
-                            `INSERT INTO users (email, password) VALUES (?,?)`,
-                            [req.body.email, hash],
-                            (error, result) => {
-                                connection.release();
-                                if (error) {
-                                    return res.status(500).send({ error: error })
-                                }
-                                response = {
-                                    message: 'User successfully created',
-                                    userCreated: {
-                                        userId: result.insertId,
-                                        email: req.body.email
-                                    }
-                                }
-                                return res.status(201).send(response);
-                            }
-                        )
-                    })
-
-                }
+        const response = {
+            message: 'User successfully created',
+            userCreated: {
+                userId: result.userId,
+                email: result.email
             }
-        )
-    });
-}
+        };
+
+        return res.status(201).send(response);
+
+    } catch (error) {
+        return res.status(500).send({
+            message: 'Created user failed',
+            error: error.message
+        });
+    }
+};
 
 exports.login = (req, res, next) => {
-    mysql.getConnection((error, connection) => {
-        if (error) {
-            return res.status(500).send({ error: error })
-        }
+    try {
+        const { foundUser } = req;
 
-        connection.query(
-            `SELECT * FROM users WHERE email = ?`,
-            [req.body.email],
-            (error, results, fields) => {
-                connection.release();
+        const token = jwt.sign({
+            userId: foundUser.userId,
+            email: foundUser.email
+        },
+            `${process.env.JWT_KEY}`,
+            {
+                expiresIn: '1h'
+            });
 
-                if (error) {
-                    return res.status(500).send({ error: error })
-                }
+        return res.status(200).send({
+            message: 'Autenticado com sucesso',
+            token: token
+        });
 
-
-                if (!req.body.email || typeof req.body.email !== 'string' || !req.body.email.trim()) {
-                    return res.status(400).json({ message: 'Email is required' });
-                }
-
-                if (!req.body.password || typeof req.body.password !== 'string' || !req.body.password.trim()) {
-                    return res.status(400).json({ message: 'Password is required' });
-                }
-
-                if (results.length === 0) {
-                    return res.status(401).send({ message: 'Invalid credentials' });
-                }
-
-                bcrypt.compare(req.body.password, results[0].password, (error, result) => {
-                    if (error) {
-                        return res.status(401).send({ message: 'Authentication failed' })
-                    }
-
-                    if (result) {
-                        const token = jwt.sign({
-                            userId: results[0].userId,
-                            email: results[0].email
-                        },
-                            `${process.env.JWT_KEY}`,
-                            {
-                                expiresIn: '1h'
-                            });
-                        return res.status(200).send({
-                            message: 'Autenticado com sucesso',
-                            token: token
-                        })
-                    }
-
-                    return res.status(401).send({ message: 'Authentication failed' })
-                })
-            })
-    })
+    } catch (error) {
+        return res.status(401).send({
+            message: 'Authentication failed',
+            error: error.message
+        })
+    }
 }
--- src/routes/user.js
+++ src/routes/user.js
@@ -1,8 +1,25 @@
 const express = require('express');
 const router = express.Router();
 const UserController = require('../controllers/user-controller');
+const { verifyLoginFields, verifyPassword, verifyLengthPassword, verifyEmailAlreadyExists } = require('../middleware/validations');
+const { handleDatabaseConnection } = require('../middleware/database');
+const { findUserByEmail } = require('../middleware/findUser');
 
-router.post('/cadastro', UserController.register);
-router.post('/login', UserController.login);
+router.post(
+    '/cadastro',
+    verifyLoginFields,
+    verifyLengthPassword,
+    handleDatabaseConnection,
+    verifyEmailAlreadyExists,
+    UserController.register);
+
+router.post(
+    '/login',
+    verifyLoginFields,
+    handleDatabaseConnection,
+    findUserByEmail,
+    verifyPassword,
+    UserController.login
+);
 
 module.exports = router;
